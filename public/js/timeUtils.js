956a1b1e443089251827eac0c5c9223cce513a46
Harshdeep Khartade <138711003+harshdeepKhartade10@users.noreply.github.com>
2024-06-08 20:11:44 +0530

    Delete public directory

--- public/js/timeUtils.js
+++
@@ -1,8 +0,0 @@
-// This function returns the current time in the Indian system
-function getCurrentTimeInIndianSystem() {
-    const now = new Date();
-    const options = { timeZone: 'Asia/Kolkata' };
-    return now.toLocaleString('en-US', options);
-}
-
-module.exports = { getCurrentTimeInIndianSystem };
